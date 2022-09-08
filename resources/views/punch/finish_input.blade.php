34ce3bce80e2b8c0197ad33a105b592a7f5ba21d
warmkatahira <t.katahira@warm.co.jp>
2022-09-08 15:37:10 +0900

    20220908_modify_4

--- resources/views/punch/finish_input.blade.php
+++ resources/views/punch/finish_input.blade.php
@@ -87,23 +87,23 @@
                     <!-- 時間入力ボタン -->
                     <div class="col-start-7 col-span-4 grid grid-cols-12" style="font-family:'Share Tech Mono'">
                         <p class="col-span-11 bg-blue-500 text-2xl text-center py-5 rounded-lg text-white" style="font-family:Zen Maru Gothic">時間入力</p>
-                        <button id="num_7" class="input_time col-span-3 bg-blue-100 text-2xl text-center py-5 rounded-lg mt-3">7<button>
-                        <button id="num_8" class="input_time col-span-3 bg-blue-100 text-2xl text-center py-5 rounded-lg mt-3">8<button>
-                        <button id="num_9" class="input_time col-span-3 bg-blue-100 text-2xl text-center py-5 rounded-lg mt-3">9<button>
-                        <button id="num_4" class="input_time col-span-3 bg-blue-100 text-2xl text-center py-5 rounded-lg mt-3">4<button>
-                        <button id="num_5" class="input_time col-span-3 bg-blue-100 text-2xl text-center py-5 rounded-lg mt-3">5<button>
-                        <button id="num_6" class="input_time col-span-3 bg-blue-100 text-2xl text-center py-5 rounded-lg mt-3">6<button>
-                        <button id="num_1" class="input_time col-span-3 bg-blue-100 text-2xl text-center py-5 rounded-lg mt-3">1<button>
-                        <button id="num_2" class="input_time col-span-3 bg-blue-100 text-2xl text-center py-5 rounded-lg mt-3">2<button>
-                        <button id="num_3" class="input_time col-span-3 bg-blue-100 text-2xl text-center py-5 rounded-lg mt-3">3<button>
+                        <button id="num_7" class="input_time col-span-3 bg-blue-100 text-4xl text-center py-5 rounded-lg mt-3">7<button>
+                        <button id="num_8" class="input_time col-span-3 bg-blue-100 text-4xl text-center py-5 rounded-lg mt-3">8<button>
+                        <button id="num_9" class="input_time col-span-3 bg-blue-100 text-4xl text-center py-5 rounded-lg mt-3">9<button>
+                        <button id="num_4" class="input_time col-span-3 bg-blue-100 text-4xl text-center py-5 rounded-lg mt-3">4<button>
+                        <button id="num_5" class="input_time col-span-3 bg-blue-100 text-4xl text-center py-5 rounded-lg mt-3">5<button>
+                        <button id="num_6" class="input_time col-span-3 bg-blue-100 text-4xl text-center py-5 rounded-lg mt-3">6<button>
+                        <button id="num_1" class="input_time col-span-3 bg-blue-100 text-4xl text-center py-5 rounded-lg mt-3">1<button>
+                        <button id="num_2" class="input_time col-span-3 bg-blue-100 text-4xl text-center py-5 rounded-lg mt-3">2<button>
+                        <button id="num_3" class="input_time col-span-3 bg-blue-100 text-4xl text-center py-5 rounded-lg mt-3">3<button>
                         <button id="all_input" class="col-span-11 bg-pink-200 text-2xl text-center py-5 rounded-lg mt-3">残り時間を全て入力<button>
                     </div>
                     <!-- 分入力ボタン -->
                     <div class="col-start-11 col-span-2 grid grid-cols-12" style="font-family:'Share Tech Mono'">
                         <p class="col-start-2 col-span-10 bg-blue-500 text-2xl text-center py-5 rounded-lg text-white" style="font-family:Zen Maru Gothic">分入力</p>
-                        <button id="num_0.25" class="input_time col-start-2 col-span-10 bg-blue-100 text-2xl text-center py-5 rounded-lg mt-3">0.25<button>
-                        <button id="num_0.50" class="input_time col-start-2 col-span-10 bg-blue-100 text-2xl text-center py-5 rounded-lg mt-3">0.50<button>
-                        <button id="num_0.75" class="input_time col-start-2 col-span-10 bg-blue-100 text-2xl text-center py-5 rounded-lg mt-3">0.75<button>
+                        <button id="num_0.25" class="input_time col-start-2 col-span-10 bg-blue-100 text-4xl text-center py-5 rounded-lg mt-3">0.25<button>
+                        <button id="num_0.50" class="input_time col-start-2 col-span-10 bg-blue-100 text-4xl text-center py-5 rounded-lg mt-3">0.50<button>
+                        <button id="num_0.75" class="input_time col-start-2 col-span-10 bg-blue-100 text-4xl text-center py-5 rounded-lg mt-3">0.75<button>
                         <button id="input_working_time_clear" class="col-start-2 col-span-10 bg-red-500 text-white text-2xl text-center py-5 rounded-lg mt-3">クリア<button>
                     </div>
                 </div>
